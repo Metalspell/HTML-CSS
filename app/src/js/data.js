e0057e276e7668b806c0e18df2b517f30f63959f
metalspell <lonelyrockclimber@gmail.com>
2021-05-09 16:12:47 +0300

    Add new unit

---
+++ app/src/js/data.js
@@ -0,0 +1,30 @@
+const data = {
+	"first_name": "Dmitry",
+	"last_name": "Krasilnikov",
+	"job_title": "Trainee Front-end Developer",
+	"photo": "images/h7pJ5tOE4W8.jpg",
+	"phone": "0938559908",
+	"email": "krasilnikov.dmytro@gmail.com",
+	"skype": "Metalspell",
+	"linkedin": "https://www.linkedin.com/in/dmitriy-krasilnikov-b9b780169",
+	"git": "https://github.com/Metalspell",
+	"address": "Dnipro",
+	"skills": [
+		"HTML",
+		"CSS (Flexbox & CSS Grid)",
+		"Bootstrap & Materialize",
+		"LESS/Grunt & SASS/Gulp",
+		"JavaScript",
+		"jQuery",
+		"ReactJS",
+		"NodeJS",
+		"Git",
+	],
+	"education": [
+		["2004 - 2009", "Dnipro National University of Railway Transport"],
+	],
+	"achievements": [
+		"Together with a friend, we wrote a website from scratch"
+	],
+	"profile": 'Hello;) You have come to the page of a beginner, but very creative and enthusiastic, and to some extent, talented web developer with a high level of determination to become a professional in this field. Life credo "Level Up!" The main task of the current stage of life is to find a starting job for a complete immersion in the world of web development.'
+};
---
+++ app/src/js/experience.js
@@ -0,0 +1,9 @@
+const experience = [
+	{
+		"company": "TiSer Dev",
+		"city": "Dnipro",
+		"position": "Front-end dev",
+		"description": "Using the knowledge gained and under the sensitive guidance of my friend Sergei, I wrote the front of his personal website, where I used the classic combination of HTML, CSS, JS. The layout is based on the Materialize (with Flex-Box) framework. Back-end written in Ruby-on-Rails",
+		"link": "https://github.com/TiSer/sergey_tiser"
+	},
+]
